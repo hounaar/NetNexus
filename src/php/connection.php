f69df14479bb33bd13ea38c8d245f5bc8eaaad9e
PARSA <147280090+parsabe@users.noreply.github.com>
2024-09-12 14:10:32 -0700

    Delete src directory

--- src/php/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$connection = new mysqli('localhost', 'root', '', 'main')  or die($connection->connect_error);
-
-?>
--- src/php/riddles/toucher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..20}
-do
-    touch "riddle$i.php"
-done
